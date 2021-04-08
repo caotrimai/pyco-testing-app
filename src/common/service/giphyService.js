db0f61d22e84561e3f02f5a3e28bbad43ff88f71
MaiCaoTri <caotri.mct@gmail.com>
2021-04-08 07:14:02 +0700

    - Show full screen feature. - load images feature. - updating styles.

--- src/common/service/giphyService.js
+++ src/common/service/giphyService.js
@@ -1,14 +1,15 @@
 const GIPHY_DOMAIN = process.env.REACT_APP_GIPHY_DOMAIN;
 const apiKey = process.env.REACT_APP_GIPHY_API_KEY;
 /*
-input: requestParams {
+input: params {
         limit: integer (int32),
         offset: integer (int32),
         rating: string
        }
 output: string url
  */
-export const getTrendingGifsURL = (limit, offset, rating) => {
+export const getTrendingGifsURL = (params) => {
+  const {limit, offset, rating} = params;
   const trendingURL = "/v1/gifs/trending";
-  return `${GIPHY_DOMAIN}${trendingURL}?api_key=${apiKey}&limit=${limit ? limit : 20}&offset=${offset && offset > 0 && offset <= 4999 ? offset : 0}${rating ? '&rating='+rating : ''}`;
+  return `${GIPHY_DOMAIN}${trendingURL}?api_key=${apiKey}&limit=${limit ? limit : 20}&offset=${offset && offset > 0 && offset <= 4999 ? offset : 0}${rating ? '&rating=' + rating : ''}`;
 };
--- src/components/ImageGridComponent/index.js
+++ src/components/ImageGridComponent/index.js
@@ -1,32 +1,73 @@
 import React, {useState, useEffect} from 'react';
 import axiosClient from "../../common/configuration/axiosClient";
 import {getTrendingGifsURL} from "../../common/service/giphyService";
+import {ImageGroup, Image} from 'react-fullscreen-image';
 import './ImageGridComponent.scss'
-import ImageComponent from "../ImageComponent";
 
-function ImageGridComponent(props) {
-  const [images, setImages] = useState([]);
+function ImageGridComponent() {
   const GIPHY_MEDIA_URL = process.env.REACT_APP_GIPHY_MEDIA_URL;
+  const [images, setImages] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [pagination, setPagination] = useState({currentPage: 0, limit: 8});
+
 
   useEffect(() => {
-    (async () => {
-      const result = await axiosClient.get(getTrendingGifsURL());
-      setImages(result.data.data.map(gif => ({id: gif.id, title: gif.title})));
-    })();
-  }, []);
+    (async () => await loadTrendingGifs())();
+  }, [])
 
-  const renderGifs = () => images.map(image =>
-    <ImageComponent
-      key={image.id}
-      src={`${GIPHY_MEDIA_URL}/${image.id}/giphy.gif`}
-      alt={image.title}
-      className="col-lg-3 col-md-4 col-sm-6"
-    />
-  );
+  const loadTrendingGifs = async () => {
+    const {currentPage, limit} = pagination;
+    const requestParams = {limit, offset: currentPage * limit};
+    setIsFetching(true);
+    try {
+      const result = await axiosClient.get(getTrendingGifsURL(requestParams));
+      setImages(images.concat(result.data.data.map(gif => ({id: gif.id, title: gif.title}))));
+      const totalCount = result.data.pagination.total_count;
+      const totalPage = totalCount % limit === 0 ? totalCount / limit : totalCount / limit + 1
+      setPagination({
+        ...pagination,
+        totalCount,
+        totalPage,
+        currentPage: currentPage + 1,
+        hasMore: totalPage === currentPage + 1
+      });
+    } catch (e) {
+      console.error(e);
+    }
+    setIsFetching(false);
+  }
+
+  const handleLoadMore = async () => {
+    await loadTrendingGifs();
+  }
+
+
+  const renderGifs = () => (
+    <ImageGroup className="image-group">
+      <div className="images">
+        {images.map(image => (
+          <div key={image.id} className="images__wrapper">
+            <Image src={`${GIPHY_MEDIA_URL}/${image.id}/giphy.gif`} alt={image.title}/>
+          </div>
+        ))}
+      </div>
+    </ImageGroup>
+  )
+
+  const renderLoadMoreBtn = () => (
+    <div className="buttons">
+      {isFetching ? <label className="loading">Loading...</label> :
+        <button className="btn btn-sm btn-load-more" onClick={handleLoadMore}>
+          Loading more
+        </button>
+      }
+    </div>
+  )
 
   return (
     <div className="ImageGridComponent">
       {renderGifs()}
+      {renderLoadMoreBtn()}
     </div>
   )
 }
